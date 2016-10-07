c3367c087bdf1c7f489ae556db7ea304889ac703
lorenzo.ita@gmail.com <lorenzo.ita@gmail.com>
2016-10-07 19:58:02 +0200

    very rudimental but it works provided that the shift is between minus and plus 26

--- solve_cipher.rb
+++ solve_cipher.rb
@@ -1,12 +1,16 @@
-def solve_cipher(input)
+
+def solve_cipher(input, shift)
   
+  shift = shift.to_i 
   letters = input.split('')
-  num = letters.map{ |n| n.ord - 1 }
-  num.map! { |x| if (x == 96); x + 26 else x = x end}
+  num = letters.map{ |n| n.ord + shift }
+  num.map! { |x| if (x - shift == 32); x = 32 else x = x end}
+  num.map! { |x| if (x <= 96 && x != 32); x + 26 else x = x end}
   output = num.map { |x| x.chr}
   puts output.join
 
   
 end
 
-solve_cipher("ifmmp")
+solve_cipher("p| uhdo qdph lv grqdog gxfn", -3)
+
