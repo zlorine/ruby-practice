3278b13808fa081d41b40d001b4f33d91871e166
lorenzo.ita@gmail.com <lorenzo.ita@gmail.com>
2016-10-07 19:06:42 +0200

    second commit includes tweak to cipher the letter a to z, not sure it0s very elegant but works. doubts remain on how to make it work for different shifts without changing the function manually

--- solve_cipher.rb
+++ solve_cipher.rb
@@ -1,9 +1,9 @@
-
 def solve_cipher(input)
   
   letters = input.split('')
   num = letters.map{ |n| n.ord - 1 }
-  output = num.map { |x| x.chr }
+  num.map! { |x| if (x == 96); x + 26 else x = x end}
+  output = num.map { |x| x.chr}
   puts output.join
 
   
